9eada1371a3ae0d6a428eec58c16d3633b07bce2
呉屋 翔大 <goyasho0831@gmail.com>
2021-05-23 14:50:17 +0900

    users追加

--- html/config/routes.php
+++ html/config/routes.php
@@ -80,11 +80,13 @@ $routes->scope('/', function (RouteBuilder $builder) {
  * ```
  * $routes->scope('/api', function (RouteBuilder $builder) {
  *     // No $builder->applyMiddleware() here.
- *     
+ *
  *     // Parse specified extensions from URLs
  *     // $builder->setExtensions(['json', 'xml']);
- *     
+ *
  *     // Connect API actions here.
  * });
  * ```
  */
+
+$routes->connect('/users', ['controller' => 'Users', 'action' => 'index']);
